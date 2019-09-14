bd6d21c358b1453defb427481226f3a7f904717d
Rudresh Sisode <rudranrajput@gmail.com>
2019-09-14 11:01:04 +0530

    Java Package Program

---
+++ Simple1.java
@@ -0,0 +1,11 @@
+package mypack;  
+public class Simple1{  
+ public void mainn(){  
+    System.out.println("Welcome to package");  
+   }  
+}  
+/*
+first step
+>javac -d . Simple1.java
+then move to the second program
+*/
---
+++ Simple2.java
@@ -0,0 +1,16 @@
+import mypack.Simple1;
+
+public class Simple2
+{
+	public static void main(String[] arvs)
+	{
+		Simple1 myobj=new Simple1();
+		myobj.mainn(); 
+	}
+}
+/*
+second step
+>javac Simple2.java
+>java Simple2
+>Welcome to package (this is output)
+*/
